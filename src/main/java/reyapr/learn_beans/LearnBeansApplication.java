c9569c1e1a291be6eabcdd038933b8e850dd4bcc
Renal Apriansyah <>
2020-02-29 20:13:00 +0700

    spring context

--- src/main/java/reyapr/learn_beans/LearnBeansApplication.java
+++ src/main/java/reyapr/learn_beans/LearnBeansApplication.java
@@ -2,12 +2,21 @@ package reyapr.learn_beans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import reyapr.learn_beans.controllers.MyController;
 
 @SpringBootApplication
 public class LearnBeansApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnBeansApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(LearnBeansApplication.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+        String greeting = myController.sayHello();
+
+        System.out.println(greeting);
+
     }
 
 }
---
+++ src/main/java/reyapr/learn_beans/controllers/MyController.java
@@ -0,0 +1,11 @@
+package reyapr.learn_beans.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        return "Hello Guys";
+    }
+}
