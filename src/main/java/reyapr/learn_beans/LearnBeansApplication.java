63d0ab40e6efc5741a80392e1dc3827d90c446f1
Renal Apriansyah <>
2020-02-29 19:51:37 +0700

    initialize springgframework

---
+++ src/main/java/reyapr/learn_beans/LearnBeansApplication.java
@@ -0,0 +1,13 @@
+package reyapr.learn_beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnBeansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reyapr/learn_beans/LearnBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package reyapr.learn_beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
